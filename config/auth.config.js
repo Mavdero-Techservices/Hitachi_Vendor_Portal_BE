092d7ef40605c7545eeffb45ffb7a76076b8e507
Vinodhini T <vinodhini.thiruvalluvar@gyanmatrix.com>
2023-02-17 10:47:16 +0530

    emailConfig

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  service: 'Outlook365',
-  host: 'smtp.office365.com',
-  port: '587',
-  user: "vms@hisysmc.com",
-  pass: "May64863",
+  // service: 'Outlook365',
+  // host: 'smtp.office365.com',
+  // port: '587',
+  // user: "vms@hisysmc.com",
+  // pass: "May64863",
+  service: 'gmail',
+  user: "karthigapalani4@gmail.com", 
+  pass: "okiqnfsjlyeemsns", 
+
 };
