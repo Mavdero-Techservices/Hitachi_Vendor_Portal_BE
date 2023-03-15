2a83bffd876e9cf8f95b644b4e65438ffc1ba84b
yellowflash123 <19uca040@gmail.com>
2023-03-15 10:30:41 +0530

    Bugs fixed

--- controller/vendorDetails.controller.js
+++ controller/vendorDetails.controller.js
@@ -15,77 +15,152 @@ exports.postVdetail = (req, res, next) => {
       const pinCode = req.body.pinCode;
       const contactName = req.body.contactName;
       const companyName = req.body.companyName;
-      const image = new Buffer(req.body.image, 'base64').toString('binary');
+      const image = req.body.image;
       const vendorType = req.body.vendorType;
       const vendorManager = req.body.vendorManager;
       const mkDenialCheque = req.body.mkDenialCheque;
       const submitStatus = req.body.submitStatus;
       const submitDate = req.body.submitDate;
-      const user = new VdetailSchema({
-        address1: address1,
-        address2: address2,
-        city: city,
-        state: state,
-        country: country,
-        pinCode: pinCode,
-        contactName: contactName,
-        companyName: companyName,
-        image: image,
-        vendorType: vendorType,
-        vendorManager: vendorManager,
-        mkDenialCheque: mkDenialCheque,
-        userId:userId,
-        submitStatus: submitStatus,
-        submitDate: submitDate
-      });
-         user.save()
-        .then((data) => {
-          return res.status(200).json({
-            status: "success",
-            data,
-            message: "Vendor details inserted Successfully",
-          });
-        })
-        .catch((err) => {
-          return res
-          .status(404)
-          .json({ status: "error", err, message: "Error Response" });
-        });
+  
+      VdetailSchema.findOne({
+        where: {
+          userId:req.body.userId,
+        },
+      })
+        .then(async user => {
+          if (!user) {
+            console.log("save api call");
+            const VendorDetails = new VdetailSchema({
+              address1: address1,
+              address2: address2,
+              city: city,
+              state: state,
+              country: country,
+              pinCode: pinCode,
+              contactName: contactName,
+              companyName: companyName,
+              image: image,
+              vendorType: vendorType,
+              vendorManager: vendorManager,
+              mkDenialCheque: mkDenialCheque,
+              userId:userId,
+              submitStatus: submitStatus,
+              submitDate: submitDate
+            });
+            VendorDetails.save()
+            .then((data) => {
+              return res.status(200).json({
+                status: "success",
+                data,
+                message: "Vendor details saved Successfully",
+              });
+            })
+            .catch((err) => {
+              return res
+              .status(404)
+              .json({ status: "error", err, message: "Error Response" });
+            });
+          }
+          else {
+console.log("call update Api:::")
+const userId = req.body.userId;
+const updates = req.body;
+
+// check if there are any empty fields
+for (const key in updates) {
+  if (!updates[key]) {
+    updates[key] = null;
+  }
+}
+
+const updateResult = await VdetailSchema.update(updates, {
+  where: { userId }
+});
+
+if (updateResult[0]) {
+  res.status(200).json({
+    status: "success",
+    message: "Vendor updated successfully",
+  });
+} else {
+  res.status(404).json({
+    status: "error",
+    message: "Vendor not found"
+  });
+}
+          }
+        })  
+        
 }
 
 //SaveVendorCommunication
 exports.SaveVendorCommunication = (req, res) => {
-  const VendorCommunication = {
-    financeSpoccontactName: req.body.financeSpoccontactName,
-    financeSpocdesignation: req.body.financeSpocdesignation,
-    financeSpocphoneNo: req.body.financeSpocphoneNo,
-    financeSpocemail: req.body.financeSpocemail,
-    operationSpoccontactName: req.body.operationSpoccontactName,
-    operationSpocdesignation: req.body.operationSpocdesignation,
-    operationSpocphoneNo: req.body.operationSpocphoneNo,
-    operationSpocemail: req.body.operationSpocemail,
-    collectionSpoccontactName: req.body.collectionSpoccontactName,
-    collectionSpocdesignation: req.body.collectionSpocdesignation,
-    collectionSpocphoneNo: req.body.collectionSpocphoneNo,
-    collectionSpocemail: req.body.collectionSpocemail,
-    managementSpoccontactName: req.body.managementSpoccontactName,
-    managementSpocdesignation: req.body.managementSpocdesignation,
-    managementSpocphoneNo: req.body.managementSpocphoneNo,
-    managementSpocemail: req.body.managementSpocemail,
-    contactName: req.body.contactName,
-    designation: req.body.designation,
-    phoneNo: req.body.phoneNo,
-    email: req.body.email,
-    userId:req.body.userId,
-    mastervendor_email:req.body.mastervendor_email,
-  };
-  vendorCommunicationDetails.create(VendorCommunication)
-    .then(data => {
-      return res.status(200).json({ msg: "success", result: data });
+  vendorCommunicationDetails.findOne({
+    where: {
+      userId:req.body.userId,
+    },
+  })
+    .then(async user => {
+      if (!user) {
+        const VendorCommunication = {
+          financeSpoccontactName: req.body.financeSpoccontactName,
+          financeSpocdesignation: req.body.financeSpocdesignation,
+          financeSpocphoneNo: req.body.financeSpocphoneNo,
+          financeSpocemail: req.body.financeSpocemail,
+          operationSpoccontactName: req.body.operationSpoccontactName,
+          operationSpocdesignation: req.body.operationSpocdesignation,
+          operationSpocphoneNo: req.body.operationSpocphoneNo,
+          operationSpocemail: req.body.operationSpocemail,
+          collectionSpoccontactName: req.body.collectionSpoccontactName,
+          collectionSpocdesignation: req.body.collectionSpocdesignation,
+          collectionSpocphoneNo: req.body.collectionSpocphoneNo,
+          collectionSpocemail: req.body.collectionSpocemail,
+          managementSpoccontactName: req.body.managementSpoccontactName,
+          managementSpocdesignation: req.body.managementSpocdesignation,
+          managementSpocphoneNo: req.body.managementSpocphoneNo,
+          managementSpocemail: req.body.managementSpocemail,
+          contactName: req.body.contactName,
+          designation: req.body.designation,
+          phoneNo: req.body.phoneNo,
+          email: req.body.email,
+          userId:req.body.userId,
+          mastervendor_email:req.body.mastervendor_email,
+        };
+        vendorCommunicationDetails.create(VendorCommunication)
+          .then(data => {
+            return res.status(200).json({ msg: "success", result: data });
+          })
+          .catch(err => {
+            return res.status(200).json({ status: 'error', data: { message: 'Error Response', err } });
+          });
+      }
+      else
+      {
+        const userId = req.body.userId;
+        const updates = req.body;
+      
+        // check if there are any empty fields
+        for (const key in updates) {
+          if (!updates[key]) {
+            updates[key] = null;
+          }
+        }
+        const updateResult = await vendorCommunicationDetails.update(req.body, {
+          where: { userId }
+        });
+      
+        if (updateResult[0]) {
+          return res.status(200).json({ msg: "success", result:"Communication details updated successfully" });
+        } else {
+          res.status(404).json({
+            msg: "error",
+            result: "Vendor not found"
+          });
+        }
+
+      }
     })
-    .catch(err => {
-      return res.status(200).json({ status: 'error', data: { message: 'Error Response', err } });
-    });
+
 };
 
 //getCountry
@@ -158,7 +233,7 @@ exports.updateCommunication = async (req, res) => {
   } else {
     res.status(404).json({
       status: "error",
-      message: "Communication details not found"
+      message: "Vendor not found"
     });
   }
 };
