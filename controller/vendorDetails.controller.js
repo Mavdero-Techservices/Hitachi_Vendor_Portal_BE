8eb290942db34c9508d5841adead4f2ac34642e6
Aravind <ajayaravind797@gmail.com>
2023-03-20 14:58:25 -0700

    Basic Info Submitted status issue Implemented.

--- controller/vendorDetails.controller.js
+++ controller/vendorDetails.controller.js
@@ -3,105 +3,103 @@ const VdetailSchema = db.vdetail;
 const ApprovalSchema = db.approvalStatus;
 const vendorCommunicationDetails = db.vendorCommunicationDetails;
 const { check, validationResult } = require("express-validator");
-var geoCountryZipCode = require('geonames-country-zipcode-lookup');
-const { getData } = require('country-list');
+var geoCountryZipCode = require("geonames-country-zipcode-lookup");
+const { getData } = require("country-list");
 
 exports.postVdetail = (req, res, next) => {
-    const userId=req.body.userId;
-      const address1 = req.body.address1;
-      const address2 = req.body.address2;
-      const country = req.body.country;
-      const state = req.body.state;
-      const city = req.body.city;
-      const pinCode = req.body.pinCode;
-      const contactName = req.body.contactName;
-      const companyName = req.body.companyName;
-      const image = req.body.image;
-      const vendorType = req.body.vendorType;
-      const vendorManager = req.body.vendorManager;
-      const mkDenialCheque = req.body.mkDenialCheque;
-      const submitStatus = req.body.submitStatus;
-      const submitDate = req.body.submitDate;
-  
-      VdetailSchema.findOne({
-        where: {
-          userId:req.body.userId,
-        },
-      })
-        .then(async user => {
-          if (!user) {
-            console.log("save api call");
-            const VendorDetails = new VdetailSchema({
-              address1: address1,
-              address2: address2,
-              city: city,
-              state: state,
-              country: country,
-              pinCode: pinCode,
-              contactName: contactName,
-              companyName: companyName,
-              image: image,
-              vendorType: vendorType,
-              vendorManager: vendorManager,
-              mkDenialCheque: mkDenialCheque,
-              userId:userId,
-              submitStatus: submitStatus,
-              submitDate: submitDate
-            });
-            VendorDetails.save()
-            .then((data) => {
-              return res.status(200).json({
-                status: "success",
-                data,
-                message: "Vendor details saved Successfully",
-              });
-            })
-            .catch((err) => {
-              return res
-              .status(404)
-              .json({ status: "error", err, message: "Error Response" });
-            });
-          }
-          else {
-console.log("call update Api:::")
-const userId = req.body.userId;
-const updates = req.body;
+  const userId = req.body.userId;
+  const address1 = req.body.address1;
+  const address2 = req.body.address2;
+  const country = req.body.country;
+  const state = req.body.state;
+  const city = req.body.city;
+  const pinCode = req.body.pinCode;
+  const contactName = req.body.contactName;
+  const companyName = req.body.companyName;
+  const image = req.body.image;
+  const vendorType = req.body.vendorType;
+  const vendorManager = req.body.vendorManager;
+  const mkDenialCheque = req.body.mkDenialCheque;
+  const submitStatus = req.body.submitStatus;
+  const submitDate = req.body.submitDate;
 
-// check if there are any empty fields
-for (const key in updates) {
-  if (!updates[key]) {
-    updates[key] = null;
-  }
-}
+  VdetailSchema.findOne({
+    where: {
+      userId: req.body.userId,
+    },
+  }).then(async (user) => {
+    if (!user) {
+      console.log("save api call");
+      const VendorDetails = new VdetailSchema({
+        address1: address1,
+        address2: address2,
+        city: city,
+        state: state,
+        country: country,
+        pinCode: pinCode,
+        contactName: contactName,
+        companyName: companyName,
+        image: image,
+        vendorType: vendorType,
+        vendorManager: vendorManager,
+        mkDenialCheque: mkDenialCheque,
+        userId: userId,
+        submitStatus: submitStatus,
+        submitDate: submitDate,
+      });
+      VendorDetails.save()
+        .then((data) => {
+          return res.status(200).json({
+            status: "success",
+            data,
+            message: "Vendor details saved Successfully",
+          });
+        })
+        .catch((err) => {
+          return res
+            .status(404)
+            .json({ status: "error", err, message: "Error Response" });
+        });
+    } else {
+      console.log("call update Api:::");
+      const userId = req.body.userId;
+      const updates = req.body;
 
-const updateResult = await VdetailSchema.update(updates, {
-  where: { userId }
-});
+      // check if there are any empty fields
+      for (const key in updates) {
+        if (!updates[key]) {
+          updates[key] = null;
+        }
+      }
 
-if (updateResult[0]) {
-  res.status(200).json({
-    status: "success",
-    message: "Vendor updated successfully",
+      const updateResult = await VdetailSchema.update(updates, {
+        where: { userId },
+      });
+
+      if (updateResult[0]) {
+        res.status(200).json({
+          status: "success",
+          message: "Vendor updated successfully",
+        });
+      } else {
+        res.status(404).json({
+          status: "error",
+          message: "Vendor not found",
+        });
+      }
+    }
   });
-} else {
-  res.status(404).json({
-    status: "error",
-    message: "Vendor not found"
-  });
-}
-          }
-        })  
-        
-}
+};
 
 //SaveVendorCommunication
 exports.SaveVendorCommunication = (req, res) => {
-  vendorCommunicationDetails.findOne({
-    where: {
-      userId:req.body.userId,
-    },
-  })
-    .then(async user => {
+  vendorCommunicationDetails
+    .findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    })
+    .then(async (user) => {
       if (!user) {
         const VendorCommunication = {
           financeSpoccontactName: req.body.financeSpoccontactName,
@@ -124,22 +122,26 @@ exports.SaveVendorCommunication = (req, res) => {
           designation: req.body.designation,
           phoneNo: req.body.phoneNo,
           email: req.body.email,
-          userId:req.body.userId,
-          mastervendor_email:req.body.mastervendor_email,
+          userId: req.body.userId,
+          mastervendor_email: req.body.mastervendor_email,
         };
-        vendorCommunicationDetails.create(VendorCommunication)
-          .then(data => {
+        vendorCommunicationDetails
+          .create(VendorCommunication)
+          .then((data) => {
             return res.status(200).json({ msg: "success", result: data });
           })
-          .catch(err => {
-            return res.status(200).json({ status: 'error', data: { message: 'Error Response', err } });
+          .catch((err) => {
+            return res
+              .status(200)
+              .json({
+                status: "error",
+                data: { message: "Error Response", err },
+              });
           });
-      }
-      else
-      {
+      } else {
         const userId = req.body.userId;
         const updates = req.body;
-      
+
         // check if there are any empty fields
         for (const key in updates) {
           if (!updates[key]) {
@@ -147,56 +149,60 @@ exports.SaveVendorCommunication = (req, res) => {
           }
         }
         const updateResult = await vendorCommunicationDetails.update(req.body, {
-          where: { userId }
+          where: { userId },
         });
-      
+
         if (updateResult[0]) {
-          return res.status(200).json({ msg: "success", result:"Communication details updated successfully" });
+          return res
+            .status(200)
+            .json({
+              msg: "success",
+              result: "Communication details updated successfully",
+            });
         } else {
           res.status(404).json({
             msg: "error",
-            result: "Vendor not found"
+            result: "Vendor not found",
           });
         }
-
       }
-    })
-
+    });
 };
 
 //getCountry
 exports.getCountry = (req, res, next) => {
   var country = getData();
   return res.status(200).json({ status: "success", data: country });
-}
+};
 //getState&cityByzipcode
 exports.getStateAndcityByzipcode = (req, res, next) => {
   var code = req.params.code;
   var pinCode = req.params.pinCode;
-  const url = `http://api.geonames.org/postalCodeLookupJSON?postalcode=${pinCode}&country=${code}&username=karthiga`
-  const axios = require('axios')
-  axios.get(url)
-    .then(result => {
+  const url = `http://api.geonames.org/postalCodeLookupJSON?postalcode=${pinCode}&country=${code}&username=karthiga`;
+  const axios = require("axios");
+  axios
+    .get(url)
+    .then((result) => {
       return res.json({ status: "success", data: result.data });
     })
-    .catch(err => console.log(err))
-  // const result = geoCountryZipCode.lookup(code,pinCode); 
+    .catch((err) => console.log(err));
+  // const result = geoCountryZipCode.lookup(code,pinCode);
   // return res.status(200).json({ status: "success", data: result });
-}
+};
 
 exports.updateVendor = async (req, res) => {
   const userId = req.params.userId;
   const updates = req.body;
-  if (req.body.submitStatus = "Submitted") {
-   const del = await ApprovalSchema.findOne({
+  if ((req.body.submitStatus === "Submitted")) {
+    const del = await ApprovalSchema.findOne({
       where: {
-        userId: userId
-      }
-    })
-    if(del){
+        userId: userId,
+      },
+    });
+    if (del) {
       ApprovalSchema.destroy({
         where: { id: id },
-      })
+      });
     }
   }
 
@@ -208,7 +214,7 @@ exports.updateVendor = async (req, res) => {
   }
 
   const updateResult = await VdetailSchema.update(updates, {
-    where: { userId }
+    where: { userId },
   });
 
   if (updateResult[0]) {
@@ -219,7 +225,7 @@ exports.updateVendor = async (req, res) => {
   } else {
     res.status(404).json({
       status: "error",
-      message: "Vendor not found"
+      message: "Vendor not found",
     });
   }
 };
@@ -235,7 +241,7 @@ exports.updateCommunication = async (req, res) => {
     }
   }
   const updateResult = await vendorCommunicationDetails.update(req.body, {
-    where: { userId }
+    where: { userId },
   });
 
   if (updateResult[0]) {
@@ -246,7 +252,7 @@ exports.updateCommunication = async (req, res) => {
   } else {
     res.status(404).json({
       status: "error",
-      message: "Vendor not found"
+      message: "Vendor not found",
     });
   }
 };
