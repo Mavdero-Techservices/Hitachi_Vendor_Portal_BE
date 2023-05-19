8777de902bcca0bf5fb8611fae1f17c572ebe0f4
yellowflash123 <karthigapalani4@gmail.com>
2023-05-19 16:15:36 +0530

    gitignore auth config

--- config/auth.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // service: 'Outlook365',
-    // host: 'smtp.office365.com',
-    // port: '587',
-    // user: "vms@hisysmc.com",
-    // pass: "May64863", 
-    // service: 'gmail',
-  apiKey:'replaceApikey'
-  };
